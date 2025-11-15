14208d19e974e1eb0fbaec7286a7231a78cee1d6
hany8382 <hany_mohamed112@icloud.com>
2025-11-15 19:55:32 +0200

    frist-version

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios';
+
+function App() {
+  const url = 'https://crudcrud.com/api/b425d3ff78d9462b9f320370cdc1c563/products';
+  async function fetchData(){
+    try{
+      const res = await axios.get(url)
+      console.log(res.data)
+    }catch(error){
+      console.log(error)
+    }
+  }
+ async function AddProduct() {
+  const new_producr={
+    title: "new product",
+    price : 656746,
+    description: "hello"
+  };
+  try{
+    const res= await axios.post(url,new_producr)
+    console.log("product Added",res.data)
+  }catch(error){
+    console.log(error)
+  }
+  
+
+  return (
+    <>
+  <button onClick={fetchData}>getdata</button>
+    <button onClick={AddProduct}>postdata</button>
+
+    
+    </>
+  )
+}
+}
+export default App
